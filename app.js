864ca14ebd08809294af71ab1605cea5b723d6f2
IsaacCH9 <carbajalchimalisaac0@gmail.com>
2025-03-10 00:31:04 -0600

    Pokemon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const app = express();
 const mysql = require("mysql2");
 require('dotenv').config();
-
 const con = mysql.createConnection({
     host: process.env.RAILWAY_DB_HOST,
     user: process.env.RAILWAY_DB_USER,
